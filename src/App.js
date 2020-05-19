1c52e99e689c16c8a2223ce6f155629d5c8426fc
khainan <muhammadrachim.k@gmail.com>
2020-05-20 00:04:07 +0700

    add animation, add envelope, add firework, install scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './main.scss';
+
+function useOnClickOutside(ref, handler) {
+  useEffect(
+    () => {
+      const listener = event => {
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+
+        handler(event);
+      };
+
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
+
+      return () => {
+        document.removeEventListener('mousedown', listener);
+        document.removeEventListener('touchstart', listener);
+      };
+    },
+    [ref, handler]
+  );
+}
 
 function App() {
+
+  const [open, setOpen] = useState(false)
+  const refEnvelope = useRef()
+
+  useOnClickOutside(refEnvelope, () => setOpen(false));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { open &&
+        <div className="pyro">
+          <div className="before"></div>
+          <div className="after"></div>
+        </div>
+      }
+
+      <div className="design">
+        <div ref={refEnvelope} className={`envelope ${open ? 'open' : ''}`} onClick={() => setOpen(true)}>
+          <div className="cover">
+            <div className="bottom"> </div>
+            <div className="left"></div>
+            <div className="right"> </div>
+            <div className="top"></div>
+          </div>
+          <div className="paper" onClick={() => {console.log("test")}}>
+            <div className="message">Love Yourself...</div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
